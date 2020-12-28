6c2883cb50bd481298dbf28406d790c8f3c33201
yishaiar <54184235+yishaiar@users.noreply.github.com>
2020-12-28 01:51:47 -0800

    auto

---
+++ python/constants.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+mm=1e-3
+gr = 1e-3
+
+D=0.08/1000 #[inch]
+h = 249*mm
+G = np.asarray([130E9,160E9])
+m=20.5*gr
+# M = 0.0753
+
+l_sides = 105*mm 
+l_center = 8*mm
+l_sides = np.asarray([l_sides,l_sides]) #  length [mm] 
+l = 2*l_sides[0] + l_center #  length [mm] 
+l /=2
+
+# l=238*mm/2
+
+kappa = (G*np.pi*D**4)/(h*32)
+# I_CM = M*((2*l)**2)/12
+I = 2*m*l**2
+# + I_CM
+# I = 0.0007853341
+T = 2*np.pi*np.sqrt(I/kappa)
+
+
+
