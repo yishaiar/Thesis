7c71f6c0f80a96ffca1ec7c78a58b79ece6e593f
yishaiar <54184235+yishaiar@users.noreply.github.com>
2020-12-30 06:44:02 -0800

    auto

--- python/dutycycle.py
+++ python/dutycycle.py
@@ -3,19 +3,27 @@ import time
 import sys
 from io import StringIO
 import matplotlib.pyplot as plt
+N=20
+x=np.arange(N)
+# y = np.zeros(N)
 
-x=np.arange(10)
 V = (x%2)*5
 
-n=100
-duty =0.1
-y = np.zeros(10)
-y[x%2 == 0]=x[x%2 == 0]/2
-y[x%2 != 0] = x[x%2 == 0]/2 +duty
+duty =0.8
+y=[]
+for i in x:
+    if i%2 == 0:
+        y.append(i/2)
+    else:
+        y.append(y[-1]+duty)
+    if i%int(N/2) == 0 and i !=0:
+        duty =0.2
+# y[x%2 == 0]=x[x%2 == 0]/2
+# y[x%2 != 0] = x[x%2 == 0]/2 +duty
 # y = y*8
 plt.step(y,V)
 plt.xlabel('time [$\mu$s]')
 plt.ylabel('voltage [V]')
-plt.title ('Duty Cycle 90%')
+# plt.title ('Duty Cycle 90%')
 plt.savefig('duty90.png')
 
