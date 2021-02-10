6a42ea868db6b9871259c3bfdf8a8a7c66f3d6d1
yishaiar <54184235+yishaiar@users.noreply.github.com>
2021-02-10 11:48:45 -0800

    auto

--- python/Q factor.py
+++ python/Q factor.py
@@ -1,32 +1,38 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-n = int(1e5)
-initaial = -5
-final = -20
-theta = 10**(np.arange(n)/n*(initaial - final)+final )
+def plot_q(n =int(1e5+1), p=1e-20):
+    initaial = -3
+    final = -20
+    theta = 10**(np.arange(n)[::-1]/(n-1)*(initaial - final)+final )
+    kappa = 2.7e-6
+    T=86
+    tau=6.8e-10
+    if isinstance(p, float):
+        p= np.ones([n])*p
+    Q = np.pi/T*( (kappa*(theta**2)) / (theta*np.pi*tau/T-p))
+    for i,q in enumerate(Q):
+        if q<0:
+            break
+    theta = theta[:i]
+    Q = Q[:i]
+    plt.plot(theta,Q)
+    
+    ones = np.ones([i])
+    plt.plot(theta,ones*0.5)
+    plt.plot(theta,ones*1)
+    plt.plot(theta,ones*0)
+    
+    plt.yscale('log')
+    plt.xscale('log')
 
-p=1e-30
-kappa = 2.7e-6
-T=86
-tau=6.8e-10
-Q = np.pi/T*( (kappa*(theta**2)) / (theta*np.pi*tau/T-p))
-for i,q in enumerate(Q):
-    if q>0:
-        break
-theta = theta[i:]
-Q = Q[i:]
+plot_q(n =int(1e5+1), p=1e-30)
         
 
-ones = np.ones([n])
-
-plt.plot(theta,Q)
-# plt.plot(theta,ones*0.5)
-# plt.plot(theta,ones*1)
-# plt.plot(theta,ones*0)
 
 
-# plt.yscale('symlog')
-# plt.xscale('log')
+
+
+
 # plt.gca().invert_xaxis()
 
