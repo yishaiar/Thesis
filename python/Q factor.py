5649b0f013941ac876c2a942858eeea2d7da7554
yishaiar <54184235+yishaiar@users.noreply.github.com>
2021-02-14 19:51:13 +0200

    auto

--- python/Q factor.py
+++ python/Q factor.py
@@ -1,33 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_q(n =int(1e5+1), p=1e-20):
-    initaial = -3
-    final = -20
-    theta = 10**(np.arange(n)[::-1]/(n-1)*(initaial - final)+final )
-    kappa = 2.7e-6
-    T=86
-    tau=6.8e-10
-    p=1E-16
-    if isinstance(p, float):
-        p= np.ones([n])*p
-    Q = np.pi/T*( (kappa*(theta**2)) / (theta*np.pi*tau/T-p))
-    for i,q in enumerate(Q):
-        if q<0:
-            break
-    theta = theta[:i]
-    Q = Q[:i]
-    plt.plot(theta,Q)
-    
-    ones = np.ones([i])
-    plt.plot(theta,ones*0.5)
-    plt.plot(theta,ones*1)
-    plt.plot(theta,ones*0)
-    
-    plt.yscale('log')
-    plt.xscale('log')
+n =int(1e5+1)
+initaial = -4
+final = -9
+theta = 2.5*10**(np.arange(n)[::-1]/(n-1)*(initaial - final)+final )
+kappa = 2.7e-6
+T=40
+tau=6.8e-10
+p=np.ones([n])*1E-18
 
-plot_q(n =int(1e5+1), p=1.58E-15)
+Q = np.pi/T*( (kappa*(theta**2)) / (theta*np.pi*tau/T-p))
+# for i,q in enumerate(Q):
+#     if q<0:
+#         break
+# theta = theta[:i]
+# Q = Q[:i]
+# ones = np.ones([i])
+
+ones = np.ones(theta.shape[0])
+plt.plot(theta,Q)
+
+plt.plot(theta,ones*0.5)
+# plt.plot(theta,ones*1)
+# plt.plot(theta,ones*1e-9)
+
+plt.yscale('log')
+plt.xscale('log')
+plt.xlabel(r'$\theta_{max}$')
+plt.ylabel(r'$\theta$')
+plt.legend(['Q factor','Critically damped'])
+
+
         
 
 
